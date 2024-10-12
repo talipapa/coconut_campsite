52ff37214d494785d4229a37c400371a479e46d0
Talipapa <davilajohn810@gmail.com>
2024-10-13 02:26:23 +0800

    Cleaning up tailwind

---
+++ OwnerMobile/app/(authenticated)/profile/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const profile = () => {
+  return (
+    <View>
+      <Text>This is a profile whtat</Text>
+    </View>
+  )
+}
+
+export default profile
