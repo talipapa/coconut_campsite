6d91ae5c69cc7efd907fa216fc379cbafead8518
Talipapa <davilajohn810@gmail.com>
2024-10-13 01:08:38 +0800

    Added tailwindcss

---
+++ OwnerMobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-3xl">
+        Hello world
+      </Text>
+    </View>
+  );
+}
