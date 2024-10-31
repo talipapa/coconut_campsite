935579e3f60d31361e124f09d839cf948592c1ce
Talipapa <davilajohn810@gmail.com>
2024-10-31 18:10:45 +0800

    Adjust cors backend

--- api_backend/config/session.php
+++ api_backend/config/session.php
@@ -156,7 +156,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'localhost'),
+    'domain' => env('SESSION_DOMAIN', request()->getHost() === 'localhost' ? 'localhost' : '.coconutcampsite.com'),
 
     /*
     |--------------------------------------------------------------------------
