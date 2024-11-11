e91cf7712bbaed28b98f3a2fa525d1d82ba6bc5d
Talipapa <davilajohn810@gmail.com>
2024-11-11 19:20:59 +0800

    Fix view-booking button

--- api_backend/resources/views/staff/rescheduled.blade.php
+++ api_backend/resources/views/staff/rescheduled.blade.php
@@ -9,7 +9,7 @@
     <p>Reference id: {{$transaction->id}}</p>
     <p>Hello, {{$booking->first_name}} {{$booking->last_name}}</p>
     <div>
-        <p>We are sorry to inform you that your booking on {{$oldDate}}has been rescheduled to {{$newDate}}.</p>
+        <p>We are sorry to inform you that your booking on {{$oldDate}} has been rescheduled to {{$newDate}}.</p>
     </div>
     <p>We hope to see you inn the designated check in soon! <a href="https://maps.app.goo.gl/opJGxp3aSqTqSxDi8">See you!</a></p>
 </body>
